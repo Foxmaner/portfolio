9f2a6a22ef30dc73181d3bdc901136591a846d89
Eskil Brännerud <eskbr129@student.liu.se>
2024-03-27 12:57:02 +0100

    added small icon list component

---
+++ portfolio/src/app/utils/data.ts
@@ -0,0 +1,16 @@
+import pythonImage from "../../../public/programming_icons/python.png";
+
+export const programming_icons = [
+    {
+      id: 1,
+      name: "Python",
+      designation: "",
+      image: pythonImage,
+    },
+    {
+      id: 2,
+      name: "C++",
+      designation: "",
+      image: pythonImage,
+    },
+  ];

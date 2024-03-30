ffaa89260df8d885c0d47a1eb844d3b8e461af01
foxmaner <eskil.brann@gmail.com>
2024-03-30 21:24:56 +0100

    changed some css

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -6,18 +6,20 @@ import { programming_icons } from "./utils/data"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Image
-        src="/portrait.jpg"
-        width={500}
-        height={500}
-        alt="Picture of the author"
-        className="rounded-full w-1/2"
-      />
-      <h1 className="w-1/2 text-center text-2xl">
+    <main className="w-full">
+      <div className="w-full">
+        <Image
+          src="/portrait.jpg"
+          width={500}
+          height={500}
+          alt="Picture of the author"
+          className="rounded-full object-cover"
+        />
+      </div>
+      <h1 className="w-full text-center">
         Eskil Brännerud
       </h1>
-      <h2 className="w-1/2 text-center text-1xl">
+      <h2 className="w-full text-center">
         Hi! And welcome to my portfolio.
       </h2>
       <div className="flex flex-row items-center justify-center mb-10 w-full">

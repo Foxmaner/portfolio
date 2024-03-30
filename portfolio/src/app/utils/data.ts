7570ab4864d86d844b59e57344fe5a8c32da2802
foxmaner <eskil.brann@gmail.com>
2024-03-30 21:08:20 +0100

    changed icons

--- portfolio/src/app/utils/data.ts
+++ portfolio/src/app/utils/data.ts
@@ -5,12 +5,36 @@ export const programming_icons = [
       id: 1,
       name: "Python",
       designation: "",
-      image: pythonImage,
+      image: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original.svg",
     },
     {
       id: 2,
       name: "C++",
       designation: "",
-      image: pythonImage,
+      image: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/cplusplus/cplusplus-original.svg",
+    },
+    {
+      id: 3,
+      name: "Java",
+      designation: "",
+      image: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/java/java-original.svg",
+    },
+    {
+      id: 4,
+      name: "C",
+      designation: "",
+      image: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/c/c-original.svg",
+    },
+    {
+      id: 5,
+      name: "React",
+      designation: "",
+      image: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg",
+    },
+    {
+      id: 6,
+      name: "SQL",
+      designation: "",
+      image: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mysql/mysql-original.svg",
     },
   ];

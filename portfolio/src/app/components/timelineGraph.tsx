407716e4d7bb58672d2350e0456187f325722a0f
Eskil Brännerud <eskil.brann@gmail.com>
2024-06-24 19:11:23 +0200

    added new data

--- portfolio/src/app/components/timelineGraph.tsx
+++ portfolio/src/app/components/timelineGraph.tsx
@@ -1,31 +1,12 @@
 import Image from "next/image";
 import TimelineEntry from "./timelineEntry";
 
-export default function timelineGraph() {
-    var data = [
-        {
-            "startDate": "Aug-2016",
-            "endDate": "Jun-2019",
-            "title": "Nacka Gymnasium",
-            "description": "Tekniska programmet med inriktning Informations och medieteknik"
-        },
-        {
-            "startDate": "Aug-2020",
-            "endDate": "Jun-2021",
-            "title": "Kungliga Tekniska Högskolan",
-            "description": "Högskoleingenjör i datateknik (Slutförde ej)"
-        },
-        {
-            "startDate": "Aug-2021",
-            "endDate": "Currently studying",
-            "title": "Kungliga Tekniska Högskolan",
-            "description": "Civilingenjör i mjukvaruteknik"
-        },
-    ];
+export default function timelineGraph({ data }: { data: any }) {
+    
   return (
     
     <div className="space-y-6 border-l-2 border-dashed border-black">
-    {data.map((item, index) => (
+    {data.map((item:any, index:any) => (
         <TimelineEntry key={index} data={item} />
     ))}
     </div>
--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -6,6 +6,42 @@ import TimelineGraph from "./components/timelineGraph";
 
 
 export default function Home() {
+  var schoolData = [
+    {
+      "startDate": "Aug-2016",
+      "endDate": "Jun-2019",
+      "title": "Nacka Gymnasium",
+      "description": "Tekniska programmet med inriktning Informations och medieteknik"
+    },
+    {
+      "startDate": "Aug-2020",
+      "endDate": "Jun-2021",
+      "title": "Kungliga Tekniska Högskolan",
+      "description": "Högskoleingenjör i datateknik (Slutförde ej)"
+    },
+    {
+      "startDate": "Aug-2021",
+      "endDate": "Currently studying",
+      "title": "Kungliga Tekniska Högskolan",
+      "description": "Civilingenjör i mjukvaruteknik"
+    },
+  ];
+
+  var experienceData = [
+    {
+      "startDate": "Aug-2019",
+      "endDate": "Jul-2020",
+      "title": "Postnord",
+      "description": "Brevbärare"
+    },
+    {
+      "startDate": "Jul-2017",
+      "endDate": "Aug-2023",
+      "title": "Glabo Gokart & Paintball",
+      "description": "Instruktör"
+    },
+  ];
+
   return (
     <main className="flex min-h-screen flex-col  justify-between p-24">
       <h1 className="font-bold text-3xl">
@@ -15,19 +51,19 @@ export default function Home() {
         Student i Mjukvaruteknik
       </h2>
       <p className=" text-1xl">
-      Hej och välkommen till min hemsida! Mitt namn är Eskil och studerar nu
-      4:e året som Civilingenjör i Mjukvaruteknik på Linköpings universitet.
+        Hej och välkommen till min hemsida! Mitt namn är Eskil och studerar nu
+        4:e året som Civilingenjör i Mjukvaruteknik på Linköpings universitet.
       </p>
-      
-      
+
+
       <div className="flex">
         <h1 className="font-bold text-2xl">
-          Erfarenheter
+          Jobb
         </h1>
-        
+
       </div>
       <div className="border-t border-2 border-black w-1/5"></div>
-      <TimelineGraph />
+      <TimelineGraph data={experienceData} />
 
 
       <div className="flex">
@@ -36,7 +72,7 @@ export default function Home() {
         </h1>
       </div>
       <div className="border-t border-2 border-black w-1/5"></div>
-      <TimelineGraph />
+      <TimelineGraph data={schoolData} />
     </main>
   );
 }

f64d28e7ccf8806206713de32d19b3494f660c87
Eskil Brännerud <eskil.brann@gmail.com>
2024-06-24 12:15:32 +0200

    added sum cool link!

--- portfolio/src/app/sidebar.tsx
+++ portfolio/src/app/sidebar.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link'
 import Image from "next/image";
 import { AnimatedTooltip } from "./components/ui/animated-tooltip";
 import { programming_icons } from "./utils/data"
+import { LinkPreview } from "./components/ui/link-preview";
 
 export default function Sidebar() {
     return (
@@ -20,18 +21,25 @@ export default function Sidebar() {
             <h3 className='font-bold'>Email</h3>
             <p>eskil.brann@gmail.com</p>
             <h3 className='font-bold'>Github</h3>
-            <p>Foxmaner</p>
+
+        
+            <LinkPreview
+                url="https://github.com/Foxmaner"
+                className="text-black">
+                <p>Foxmaner</p>
+            </LinkPreview>
+            
             <h3 className='font-bold'>CV</h3>
             <p>Ladda ner här!</p>
-            
+
             <h1 className='font-bold text-2xl'>Programmeringsspråk</h1>
-            
+
             <div className="border-t border-2 border-black mx-auto w-1/2 m-2"></div>
 
             <div className="flex flex-row items-center justify-center mb-10 w-full">
                 <AnimatedTooltip items={programming_icons} />
             </div>
-            
+
             <h1 className='font-bold text-2xl'>Färdigheter</h1>
             <div className="border-t border-2 border-black mx-auto w-1/2 m-2"></div>
         </header>
